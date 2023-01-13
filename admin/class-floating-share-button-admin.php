042f0effd324a9271020f224b38ab425f5f03e8d
Bowo <wibowosulistio@gmail.com>
2023-01-13 13:36:48 +0700

    Dequeue empty admin.js

--- admin/class-floating-share-button-admin.php
+++ admin/class-floating-share-button-admin.php
@@ -96,7 +96,7 @@ class Floating_Share_Button_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/floating-share-button-admin.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/floating-share-button-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
